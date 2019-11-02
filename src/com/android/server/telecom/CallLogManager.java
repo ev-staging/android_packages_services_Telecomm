653cb8fa2335b56f775c9719e3e79f95109f77fb
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-02 18:52:04 +0100

    Telecom: Add sensitive phone numbers hooks
    Change-Id: I809ceb65d7e188eb09dd4d85b5fdd716c63c60ef
    
    Telecom: Adapt to the new location of SensitivePhoneNumbers
    
    * For an explanation, see the other patch(es) of the topic
    
    Change-Id: I45f2372dcf3aa20a2606bfb91e3763e2e372d0fc
    
    Telecom: Convert to using singleton class
    
    * SenstivePhoneNumbers is now a singleton, so change the
      calls accordingly
    
    Change-Id: I6377f42e4b9c11c0e74345cc65d50476d3c911ba

--- src/com/android/server/telecom/CallLogManager.java
+++ src/com/android/server/telecom/CallLogManager.java
@@ -45,6 +45,8 @@ import com.android.internal.telephony.CallerInfo;
 import com.android.internal.telephony.SubscriptionController;
 import com.android.server.telecom.callfiltering.CallFilteringResult;
 
+import com.evervolv.internal.phone.SensitivePhoneNumbers;
+
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Objects;
@@ -145,6 +147,7 @@ public final class CallLogManager extends CallsManagerListenerBase {
 
     private Object mLock;
     private String mCurrentCountryIso;
+    private SensitivePhoneNumbers mSensitivePhoneNumbers;
 
     public CallLogManager(Context context, PhoneAccountRegistrar phoneAccountRegistrar,
             MissedCallNotifier missedCallNotifier) {
@@ -154,6 +157,7 @@ public final class CallLogManager extends CallsManagerListenerBase {
         mPhoneAccountRegistrar = phoneAccountRegistrar;
         mMissedCallNotifier = missedCallNotifier;
         mLock = new Object();
+        mSensitivePhoneNumbers = SensitivePhoneNumbers.getInstance();
     }
 
     @Override
@@ -396,9 +400,13 @@ public final class CallLogManager extends CallsManagerListenerBase {
                     CarrierConfigManager.KEY_ALLOW_EMERGENCY_NUMBERS_IN_CALL_LOG_BOOL);
         }
 
+        // Don't log sensitive numbers.
+        boolean isSensitiveNumber = mSensitivePhoneNumbers.isSensitiveNumber(mContext, number,
+                mPhoneAccountRegistrar.getSubscriptionIdForPhoneAccount(accountHandle));
+
         // Don't log emergency numbers if the device doesn't allow it.
         final boolean isOkToLogThisCall = (!isEmergency || okToLogEmergencyNumber)
-                && !isUnloggableNumber(number, configBundle);
+                && !isUnloggableNumber(number, configBundle) && !isSensitiveNumber;
 
         sendAddCallBroadcast(callType, duration);
 
